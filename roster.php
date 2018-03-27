a1e25fbce256f0c8c8823a1868fe444785766a44
Bafometa <35770842+bafometa@users.noreply.github.com>
2018-03-27 12:38:13 +0300

    Create roster.php

---
+++ roster.php
@@ -0,0 +1,238 @@
+	<div class="section">
+		<h1><span>Ростер</span></h1>
+		<div class="container">
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#wellcome]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#navig]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500); return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#link-block]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);	var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#news]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500); return false;});});</script>
+
+<div id="pun-pageulinks"><ul class="container">
+<li><a href="#wellcome" class="some_link">О званиях</a></li>
+<li><a href="#navig" class="some_link">Совет гильдии</a></li>
+<li><a href="#news" class="some_link">Состав статика</a></li>
+</ul></div>
+
+<!-- верхний текст -->
+<div id="text-block" style="height: 450px;">
+<a name="wellcome"></a>
+<span class="up-h2">О званиях и должностях</span>
+<h2>Герои гильдии «Синие Ходоки»</h2>
+ В гильдии «Синие Ходоки» звания - не пустой атрибут и не формальность. Это должность, которая несет не только статус и привилегии, но и обязательства перед гильдией.<br/>
+ К сожалению, игра ограничивает наши возможности по созданию более расширенной системы званий, которая отражала бы более четкую картину занимаемых должностей. Например, Лейтенантом в гильдии может значиться как игрок, занимающийся рекрутингом или сайтом гильдии, так и непосредственно РейдЛидер одного из статиков.<br/>
+Поэтому поэтому мы и организовали полноценную страницу с ростером нашей гильдии, дающую вам максимальную информацию о игроках, которые могут помочь вам в тех или иных аспектах игры в нашей гильдии.
+</div>
+<hr class="big-hr1">
+
+<!-- Рейд Лидеры -->
+<div id="text-block" style="padding-top: 55px;">
+    <a name="navig"></a>
+    <h2>Совет гильдии</h2>
+</div>
+
+<div id="tab-block" style="margin-top: -30px;">
+
+
+<!-- меню -->
+  <ul class="tabs">
+    <li class="active" id="pic">
+      <a href="#tab1" class="no-act"><img src="http://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+      <a href="#tab1" class="act tip" title="Морксид: Глава гильдии, Лидер PvE направления, РЛ статика"><img src="http://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+    </li>
+	 <li class="active" id="pic">
+      <a href="#tab2" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/119/137405047-avatar.jpg"></a>
+      <a href="#tab2" class="act tip" title="Берелион: Совет Гильдии, Помошник РЛ"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/119/137405047-avatar.jpg"></a>
+    </li>
+  </ul>
+
+  <div class="tab_container1">
+
+<!-- Бафомета -->
+    <div style="display: block;" id="tab1" class="tab_content">
+
+<div id="str">
+
+<div id="info-str11">
+<div id="info-str1">
+<span class="up-h2">Глава гильдии</span>
+<h3>Бафомета</h3>
+<a href="http://bluewallkers.topbb.ru/profile.php?id=2">Профиль</a> • <a href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%91%D0%B0%D1%84%D0%BE%D0%BC%D0%B5%D1%82%D0%B0" target="_blank">Армори</a><br/>
+BattleTag: morana#2326<br/>
+</div>
+<div id="info-str2">
+Деятельность: 
+<span>Глава Гильдии<br/>
+Лидер PvE направления<br/>
+Разработка сайта и видео</span>
+В рейде я - воплощение ненависти. Если у меня не дергается левый глаз и не трясуцо руки - значит просто не я РЛю рейдом. Я ненавижу всех членов рейда. Всех по-разному и всех за разное - все помню, записываю и штрафую.<br/>
+Вне рейда я - добрая, общительная, внимательная к проблемам каждого подопечного. Мне абсолютно пофиг кто чем занимается вне рейда, но если нужна помощь помогаю.<br/>
+</div>
+</div>
+<div id="info-str3"><b>Имя:</b> Мила.<br>
+Бафомета возглавила гильдию «Синие Ходоки» в 2017-м году. Организовала PvE-направление гильдии, Руководит статиком.<br> 
+Помимо РЛства PvЕ-состава также берется за проведение и организацию других мероприятий в любых аспектах игры по мере их востребованности.<br>
+Как ГМ также заведует рекрутингом, обеспечением гильдии и продвижением. Администрирует сайт гильдии.<br>
+<b>К Бафомете можно обращаться по вопросам:</b> PvE направления, статика, вступления в гильдию, назначением на должность РЛа любого направления или текущих вакансий гильдии, спонсирования и услуг гильдии.</div>
+<img class="foto" src="https://pp.userapi.com/c633830/v633830771/31217/9NTI3bPVJng.jpg">
+</div>
+
+    </div>
+
+
+    </div>
+  <div class="tab_container1">
+
+<!-- Берелион -->
+    <div style="display: block;" id="tab2" class="tab_content">
+
+<div id="str">
+
+<div id="info-str11">
+<div id="info-str1">
+<span class="up-h2">Совет гильдии</span>
+<h3>Берелион</h3>
+<a href="http://bluewallkers.topbb.ru/profile.php?id=2">Профиль</a> • <a href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%B1%D0%B5%D1%80%D0%B5%D0%BB%D0%B8%D0%BE%D0%BD" target="_blank">Армори</a> <br/>
+BattleTag: <br/>
+</div>
+<div id="info-str2">
+Деятельность: 
+<span>Совет Гильдии<br/>
+Помошник РЛ-а<br/>
+
+бла-бла-бла<br/>
+</div>
+</div>
+<div id="info-str3"><b>Имя:</b> Дима.<br>
+Пришел в гильдию спустя пару недель после её основания. И очень быстро закрепился в ней.<br> 
+Помимо хождения со статиком также берется за участие в других мероприятиях и любых аспектах игры по мере их востребованности.<br>
+Как Член Совета Гильдии также заведует рекрутингом, обеспечением гильдии и продвижением.<br>
+<b>К Берелиону можно обращаться по вопросам:</b> статика, вступления в гильдию, назначением на должность РЛа любого направления или текущих вакансий гильдии, спонсирования и услуг гильдии.</div>
+<img class="foto" src="https://pp.userapi.com/c604426/v604426922/13ffa/AbeB2nF0lgQ.jpg">
+</div>
+
+    </div>
+
+
+    </div>
+<!-- конец -->
+  </div>
+</div>
+<hr class="big-hr2">
+
+<div id="r1-block"><div id="r1-inner">
+<div id="text-block" style="padding-top: 70px;">
+    <a name="news"></a>
+    <h2>Состав статика</h2>
+</div>
+
+<div id="r1-item">
+<a id="prof" href="#" class="tip"  title="Профиль" style="color: #c41e3b !important">Бафомета</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%A5%D0%BE%D0%BF%D1%81%D1%8C" class="act tip" title="армори" target="_blank"><img src="http://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="http://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/41048.png" title="Блад ДК">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?id=6" class="tip"  title="Профиль" style="color: #fff468 !important">Берелион</a>
+<a id="pic" href="http://eu.battle.net/wow/ru/character/%D0%B3%D0%B0%D0%BB%D0%B0%D0%BA%D1%80%D0%BE%D0%BD%D0%B4/%D0%91%D0%B5%D1%80%D0%B5%D0%BB%D0%B8%D0%BE%D0%BD/" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/119/137405047-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/119/137405047-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/91162.png" title="Рога Ликвидация">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?id=4" class="tip"  title="Профиль" style="color: #2359ff !important">Миснари</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%9C%D0%B8%D1%81%D0%BD%D0%B0%D1%80%D0%B8" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/247/135751927-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/247/135751927-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/56825.png" title="Шам элем">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="#" class="tip"  title="Профиль" style="color: #aad372 !important">Воданаз</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%92%D0%BE%D0%B4%D0%B0%D0%BD%D0%B0%D0%B7" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/246/79364598-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/246/79364598-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/42137.png" title="Хант БМ">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?section=essentials&id=3" class="tip"  title="Профиль" style="color: #ff7c0a !important">Паридия</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%9F%D0%B0%D1%80%D0%B8%D0%B4%D0%B8%D1%8F" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/220/120076508-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/220/120076508-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/74897.png" title="Рестор дру">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?id=7" class="tip"  title="Профиль" style="color: #a330c9 !important">Фириран</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%A4%D0%B8%D1%80%D0%B8%D1%80%D0%B0%D0%BD" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/162/137857442-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/162/137857442-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/68128.png" title="ДХ дд">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="#" class="tip"  title="Профиль" style="color: #f48cba !important">Херасекак</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%A5%D0%B5%D1%80%D0%B0%D1%81%D0%B5%D0%BA%D0%B0%D0%BA" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/241/138159857-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/241/138159857-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/70626.png" title="Холи пал">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?id=9" class="tip"  title="Профиль" style="color: #aad372 !important">Хопсь</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/galakrond/%D0%A5%D0%BE%D0%BF%D1%81%D1%8C" class="act tip" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/215/137337559-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/215/137337559-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/42137.png" title="Хант ММ">
+</div>
+
+<div id="r1-item">
+<a id="prof" href="#" class="tip"  title="Профиль" style="color: #c69b6d!important">Мисариан</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/gordunni/%D0%BC%D0%B8%D1%81%D0%B0%D1%80%D0%B8%D0%B0%D0%BD" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/229/172348389-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/229/172348389-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/69452.png" title="Вар фури">
+</div> 
+
+<div id="r1-item">
+<a id="prof" href="http://bluewallkers.topbb.ru/profile.php?id=8" class="tip"  title="Профиль" style="color: #c69b6d!important">Ванака</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/gordunni/%D0%BC%D0%B8%D1%81%D0%B0%D1%80%D0%B8%D0%B0%D0%BD" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/229/172348389-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/229/172348389-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/69452.png" title="Вар фури">
+</div> 
+
+<div id="r1-item">
+<a id="prof" href="#" class="tip"  title="Профиль" style="color: #68ccef!important">Бабаяага</a>
+<a id="pic" href="https://worldofwarcraft.com/ru-ru/character/gordunni/%D0%B1%D0%B0%D0%B1%D0%B0%D1%8F%D0%B0%D0%B3%D0%B0" title="армори" target="_blank"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/165/170349989-avatar.jpg"></a>
+<a id="pic" href="#" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/gordunni/165/170349989-avatar.jpg"></a>
+<img id="class" class="tip" src="http://forumfiles.ru/files/0019/33/10/18919.png" title="Фрост маг">
+</div> 
+
+
+</div></div>
+
+<!-- Скрипт вкладок  -->
+<script type="text/javascript">
+$(document).ready(function() {
+  $(".tab_content").hide();  
+  $("ul.tabs li:first").addClass("active").show();
+  $(".tab_content:first").show();   
+  $("ul.tabs li").click(function() {
+  $("ul.tabs li").removeClass("active");
+  $(this).addClass("active");
+  $(".tab_content").hide();
+  var activeTab = $(this).find("a").attr("href");
+  $(activeTab).fadeIn();
+  return false;
+  });
+});
+</script>
+<style>#pun-pages .main {height: 2000px;}
+#tab1 {background: rgba(0, 0, 0, 0) url("http://forumfiles.ru/files/0019/33/10/89622.jpg") no-repeat scroll 0 0;}
+#tab2 {background: #060604 url("http://forumfiles.ru/files/0019/33/10/13620.jpg") no-repeat scroll 0 0;}
+#tab-block .tab_content {padding-bottom: 40px;min-height: 530px;margin-top: -30px;
+    margin: 0 auto;
+    position: relative;
+    text-align: center;
+    font-family: "Open Sans",Arial,Helvetica,sans-serif;
+    font-weight: 400;
+    font-size: 1.7rem;
+    line-height: 1.5em;
+    color: #fff;
+    text-shadow: 0 0 1px transparent, 0 1px 2px rgba(0,0,0,.8);}
+</style>
+		</div>
+	</div>
