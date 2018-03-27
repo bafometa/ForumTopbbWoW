37eff9252bef5df9bf69838bcb29662925925608
Bafometa <35770842+bafometa@users.noreply.github.com>
2018-03-27 12:37:11 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,209 @@
+<div id="pun-main" class="main pages">
+	<div class="section">
+		<h1><span>Гильдия Синие Ходоки - Гордунни (World of Warcraft)</span></h1>
+		<div class="container">
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#wellcome]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#navig]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500); return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#link-block]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);	var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#news]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500); return false;});});</script>
+
+
+<div id="pun-pageulinks"><ul class="container">
+<li><a href="#wellcome" class="some_link">Добро пожаловать!</a></li>
+<li><a href="#navig" class="some_link">Навигация по сайту</a></li>
+<li><a href="#link-block" class="some_link">Ссылки и информация</a></li>
+<li><a href="#news" class="some_link">Новости гильдии</a></li>
+</ul></div>
+
+<!-- верхний текст -->
+<div id="text-block" style="height: 400px;">
+<a name="wellcome"></a>
+<span class="up-h2">Гордунни</span>
+<h2>Гильдия «Синие Ходоки»</h2>
+Ходоки – наша семья, убежище, спасение от теней прошлого, <br>
+место, где ты становишься новым человеком.<br/>
+Мы сильны и упрямы. Орудиями нашими будут показуха и ремесло. <br/>
+Девиз Ходоков – максимальная эффективность при минимальном риске.<br/>
+Друзей у нас нет. Все прочие – враги или просто недостойны доверия.<br/>
+Мы отрекаемся от ярлыков. Добро и зло – категории, не имеющие к нам отношения. <br/>
+Мы просто делаем свое дело и стараемся быть лучшими в нем. Без моральных дилемм и соплей.
+</div>
+<hr class="big-hr1">
+
+<!-- навигация -->
+<div id="navig-bg">
+<div id="text-block" style="padding-top: 60px">
+<a name="navig"></a>
+<span class="up-h2">Навигация по сайту</span>
+<h2>Информация для новичков</h2></div>
+
+<div id="navig-content">
+
+<div id="col" class="leblock"><h4>Новичкам и гостям</h4>
+<div id="wtfblock" class="pve"><b>Статик</b><br>
+<a href="http://bluewallkers.topbb.ru/pages/rekruting#news" target="_blank">Информация о статике и РТ</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=18" target="_blank">Правила статиков</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=27" target="_blank">Подать заявку</a> </div>
+
+<div id="wtfblock" class="rlwtf"><b>РЛам</b><br>
+<a href="http://bluewallkers.topbb.ru/pages/rekruting#link-block" target="_blank">Информация для РЛов</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=21" target="_blank">Подать заявку на Должность</a> 
+<a href="http://bluewallkers.topbb.ru/viewforum.php?id=7" target="_blank">Другие вакансии гильдии</a> </div>
+</div>
+
+<div id="col" class="riblock"><h4>Согильдейцам</h4>
+<div id="wtfblock" class="pop"><b>Популярные запросы</b><br>
+<a href="http://bluewallkers.topbb.ru/search.php?action=show_recent" target="_blank">Последние сообщения на форуме</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=8" target="_blank">Сообщения об отсутствии</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=6" target="_blank">Получение доступа к скрытым форумам</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=11" target="_blank">Гайд по Discord</a> 
+<a href="http://bluewallkers.topbb.ru/pages.php?page=guild#link-block" target="_blank">Как получать звания?</a> </div>
+<div id="wtfblock" class="help"><b>Чем помочь гильдии?</b><br>
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=17" target="_blank">Вакансия: Рекрутер</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=23" target="_blank">Вакансия: Видеомейкер</a> 
+<a href="http://bluewallkers.topbb.ru/viewtopic.php?id=22" target="_blank">Вакансия: Модератор</a> 
+<a href="http://bluewallkers.topbb.ru/viewforum.php?id=7" target="_blank">И куча другой деятельности</a> </div>
+</div>
+
+<div id="col" class="centblock"><h4>Навигация по сайту</h4>
+<a href="http://bluewallkers.topbb.ru/=&forum" target="_blank">Форум: новости, заявки, общение и куча гайдов</a>
+<a href="http://bluewallkers.topbb.ru/pages.php?page=roster" target="_blank">Ростер: информация о РЛах</a>
+<a href="http://bluewallkers.topbb.ru/pages.php?page=guild" target="_blank">Все о гильдии: чем мы отличаемся, правила, традиции, история</a>
+<a href="http://bluewallkers.topbb.ru/pages/rekruting" target="_blank">Рекрутинг: о статиках, гильдии и требованиях к игрокам</a>
+<a href="http://bluewallkers.topbb.ru/pages.php?page=media" target="_blank">Медиа: скрины, видео, стримы</a>
+
+</div>
+
+</div>
+
+</div>
+<hr class="big-hr2">
+
+<!-- ссылки -->
+<div id="link-block">
+
+<span a="link-block"></a>
+
+<div id="text-block" style="padding-top: 65px;">
+<span class="up-h2">Ссылки</span>
+<h2>Логи, армори и прочее</h2></div>
+
+
+<div id="leftlenk-block">
+<span>Армори гильдии</span>
+<a class="tip" target="_blank" title="Оружейная" href="http://eu.battle.net/wow/ru/guild/gordunni/%D0%A1%D0%B8%D0%BD%D0%B8%D0%B5_%D0%A5%D0%BE%D0%B4%D0%BE%D0%BA%D0%B8/"><img src="http://sa.uploads.ru/q2HgO.png" alt="" /></a> 
+<span>Страница на Wowprogress</span>
+<a class="tip" target="_blank" title="Мы на ВоВ-прогрессе" href="https://www.wowprogress.com/guild/eu/%D0%B3%D0%BE%D1%80%D0%B4%D1%83%D0%BD%D0%BD%D0%B8/%D0%A1%D0%B8%D0%BD%D0%B8%D0%B5+%D0%A5%D0%BE%D0%B4%D0%BE%D0%BA%D0%B8"><img style="margin-right: 20px;" src="http://sa.uploads.ru/Tvj18.png" alt="" /></a> 
+<span>Канал на Youtube</span>
+<a class="tip" target="_blank" title="Наши видосы с Youtube" href="https://www.youtube.com/channel/UCImgqZedOhQtuzI_mZpUQcw"><img src="http://s6.uploads.ru/zSDoO.png" alt="" /></a> 
+<span>Мы вконтакте</span>
+<a class="tip" target="_blank" title="Наш паблик Вконтакте" href="https://vk.com/bluewalkersalalliance"><img src="http://forumfiles.ru/files/0019/33/10/89359.png" alt="" /></a> 
+</div>
+<div id="Teamspeak-block">
+<a class="tip" target="_blank" title="Скачать Discord" href="https://discordapp.com/download"><img src="http://sa.uploads.ru/kQ2cv.png" alt="" /></a>
+<span class="download">Войс чат: discord.gg/JAdgNJm
+<br/>
+Инструкция с картинками: <a href="http://bluewallkers.topbb.ru/viewtopic.php?id=11">Гайд по настройке Discord</a>
+</div>
+<div id="prog-block">
+<span>Прогресс</span><br>
+<a href="https://wowprogress.com/"><img alt="WoW Guild Rankings" src="https://wowprogress.com/guild_img/1531501/out/type.site" border="0"/></a>
+<iframe src="https://wowtrack.org/guilds/EU/Gordunni/%D0%A1%D0%B8%D0%BD%D0%B8%D0%B5%20%D0%A5%D0%BE%D0%B4%D0%BE%D0%BA%D0%B8?response=combined" style="overflow: hidden; height:160px"></iframe>
+<iframe src="https://wowtrack.org/guilds/EU/Gordunni/%D0%A1%D0%B8%D0%BD%D0%B8%D0%B5%20%D0%A5%D0%BE%D0%B4%D0%BE%D0%BA%D0%B8?response=bosses" style="overflow: hidden;height:280px"></iframe>
+</div>
+
+<div id="logs-block">
+<span>Логи</span>
+<a class="tip" target="_blank" title="Наши логи" href="https://www.warcraftlogs.com/guilds/314101/"><img src="http://sa.uploads.ru/6NbaE.png" alt="" /></a>
+<iframe src="http://www.warcraftlogs.com/guilds/recent_reports/314101/"></iframe>
+</div>
+
+<div id="vid-block">
+<video autoplay="autoplay" loop="loop" width="2260">
+<source src="https://bnetcmsus-a.akamaihd.net/cms/template_resource/s3/S3Q36Q7Z4HHY1509577904719.mp4" type='video/webm; codecs="vp8, vorbis"' />
+</video>
+<img src="http://media.blizzard.com/wow/legion-6a153ad2/images/world-dissolve-still.jpg">
+</div>
+
+</div>
+<hr class="big-hr1">
+
+<!-- новости -->
+<div id="text-block" style="margin-top: 55px; height: 500px;">
+<a name="news"></a>
+<script type="text/javascript">
+var arr=document.getElementsByTagName("div")
+i=0
+while(arr ){
+if(arr[i].id=="u-panel" && GroupID == "3") {
+name=arr[i].innerHTML
+name=name.substring(0)
+arr[i].innerHTML="<div id=option><span class='g-log'><b>Привет, Гость!</b></span><span class='u-ava'><img src=http://forumfiles.ru/files/0012/ad/ab/72354.jpg></span><div class='ug-data'>Регистрация позволит оставлять сообщения на форуме, прикрепить оружейную к профилю и подать заявки на вступление в гильдию или статики.<br/><br/>Незарегистрированные пользователи могут задать свои вопросы в разделе <a href='/viewforum.php?id=12'>Гостевая книга</a>.</div><div class='ug-mobile'><a href='login.php'>Вход</a><a href='register.php'>Регистрация</a></div></div>"+name
+}i++}
+</script>
+<span class="up-h2">Последние новости гильдии</span>
+<div class="lp-cont"></div>
+    <script src="http://bluewallkers.topbb.ru/export.php?type=js&max=6" type="text/javascript"></script>
+    <script type="text/javascript">
+    var html='';
+    var ts = '';
+    var mo = '';
+    var hh = '';
+    var mm = '';
+    var i = 0;
+    for(i=0;i<content.length;i++) {
+    ts = new Date();
+    ts.setTime(1000*content[i][0]);
+    mo = ts.getMonth()+1;
+    if (mo<10) mo='0'+mo;
+    hh = ts.getHours();
+    mm = ts.getMinutes()+'';
+    if (mm<10) mm='0'+mm;
+    ts = ts.getDate()+'.'+mo+' '+hh+':'+mm;
+    html2="</tr></tbody></table></div>"
+    html1="<div class=\"lp-table\"><table cellspacing=\"3\" width=100%><tbody class=\"hasicon\">"
+    html+='<tr><td colspan=\"2\"><a href=\"'+content[i][2]+'\" target=\"_blank\">'+content[i][3]+'</a><br><b>'+content[i][1]+'</b> - '+ts+'</td></tr>';
+    }
+
+    var arr=document.getElementsByTagName("div")
+    i=0
+    str=document.URL
+    while(arr[i] ){
+    if(document.getElementById("pun-index") && arr[i].className=="lp-cont"){
+    name=arr[i].innerHTML
+    name=name.substring(0)
+    arr[i].innerHTML=""+html1+""+html+""+html2+""+name+""
+    }
+    i++
+    }
+    </script>
+<a href="http://bluewallkers.topbb.ru/search.php?action=show_recent" id="allp">Все сообщения за сутки</a> </div>
+ </div>
+
+
+
+<style>#pun-pages .main {height: 2640px;}</style>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">$(document).trigger("pun_main_ready");</script>
+
+<!-- pun_stats -->
+
+<!-- banner_mini_bottom -->
+
+<!-- banner_bottom -->
+
+<div id="pun-break3" class="divider"><hr /></div>
+
+<div id="pun-crumbs2" class="section">
+	<p class="container crumbs"><strong>Вы здесь</strong> » <a href="http://bluewallkers.topbb.ru/">Сайт гильдии «Синие Ходоки» (World of Warcraft) Гордунни</a> » Гильдия Синие Ходоки - Гордунни (World of Warcraft)</p>
+</div>
+
+<div id="pun-break4" class="divider"><hr /></div>
+
+<div id="pun-about" class="section">
+	<p class="container">
+	</p>
+</div>
+<script type="text/javascript">$(document).trigger("pun_about_ready");</script>
