16ce831e9b8e5ec4b7684687da9cdf2d671b5505
Bafometa <35770842+bafometa@users.noreply.github.com>
2018-03-27 12:39:36 +0300

    Create announcement.php

---
+++ announcement.php
@@ -0,0 +1,289 @@
+<h2><span>Объявление</span></h2>
+	<div class="container">
+		<div class="html-box">
+			<!--- СЛАЙДЕР --->
+<div id="slideshow">
+    <div id="slidesContainer">
+
+      <div class="slide">
+
+<h3>Рекрутинг в статик</h3>
+РТ статика: ср, чт, сб 19:45-22:00. Прогресс: <b>8</b>/9 Гер. <font color=#ff7c0a>Внимание!</font> Мы всегда готовы рассмотреть игроков с ЛУЧШИМИ показателями, чем у наших рейдеров, даже если ваш класс не востребован. <a class="tip" title="подать заявку" href="/viewtopic.php?id=27">Подавайте заявки</a> - ответим, обсудим.
+<div id="recruit">
+<div id="heal">
+<!-- ХИЛЫ -->
+
+<a href="/viewtopic.php?id=27" class="tip" title="Разыскивается ДЦ"><div id="prist" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="dru" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="monk" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="pal" class="green"></div></a>
+<a href="/viewtopic.php?id=27 class="tip" title="ОЧЕНЬ нужен ршам"><div id="sham" class="red"></div></a>
+
+</div>
+<div id="tank">
+<!-- ТАНКИ -->
+
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="war" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="ОЧЕНЬ нужен"><div id="dru" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="monk" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="pal" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="dk" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="dh" class="green"></div></a>
+
+</div>
+<div id="dd">
+<!-- ДД -->
+
+<a href="/viewtopic.php?id=27" class="tip" title="Нужны армс/фури."><div id="war" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="ОЧЕНЬ ждем СОВУ!"><div id="dru" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Ищем ШП!"><div id="prist" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Ищем МАГА!"><div id="mag" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Рассмотрим при наличии фиолетовых логов и возможности играть ДВУМЯ чарами."><div id="monk"  class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="hant" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="не востребован"><div id="pal" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Нужен ШД рога"><div id="rog" class="red"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Рассмотрим при наличии фиолетовых логов и возможности играть ДВУМЯ чарами."><div id="dk" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Рассматриваем в состав"><div id="lok" class="yellow"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Рассмотрим при наличии фиолетовых логов и возможности играть ДВУМЯ чарами."><div id="sham" class="green"></div></a>
+<a href="/viewtopic.php?id=27" class="tip" title="Рассмотрим при наличии фиолетовых логов и возможности играть ДВУМЯ чарами."><div id="dh" class="green"></div></a>
+
+</div>
+</div>
+
+
+      </div>
+
+
+      <div class="slide">
+
+<h3>Вакансии</h3>
+Гильдия разыскивает желающих занять высокие и почетные должности! Все вакансии влекут за собой не только выполнение ряда обязательств перед гильдией, но и бонусы, награды, выплаты и рост по званию. <br><br>
+
+<a id="vak" class="tip" title="Подать заявку на должность Рейд Лидера: актуальные вакансии для РЛов, бонусы, условия" href="/viewtopic.php?id=21"><img src="http://wow.zamimg.com/images/wow/icons/medium/ability_toughness.jpg"> <span>Рейд Лидер</span></a>
+
+<a id="vak" class="tip" title="Подать заявку на должность Рекрутера: подробно о бонусах, обязанностях и условиях" href="/viewtopic.php?id=17"><img src="http://wow.zamimg.com/images/wow/icons/medium/ability_hunter_snipershot.jpg"> <span>Рекрутер</span></a>
+
+<a id="vak" class="tip" title="Подать заявку на должность Аналитика Логов: подробнее о необходимой работе и о бонусах для желающего ее выполнять" href="/viewtopic.php?id=24"><img src="http://wow.zamimg.com/images/wow/icons/medium/achievement_ashran_tourofduty.jpg"> <span>Аналитик логов</span></a>
+
+<a id="vak" class="tip" title="Подать заявку на должность Модератора форума: подробнее о необходимой работе, требованиях, выплатах и бонусах для модераторов" href="/viewtopic.php?id=22"><img src="http://wow.zamimg.com/images/wow/icons/medium/achievement_faction_lorewalkers.jpg"> <span>Модератор</span></a>
+
+<a id="vak" class="tip" title="Подать заявку на должность Видеомейкера: подробнее о вакансии и бонусах для видеомейкеров" href="/viewtopic.php?id=23"><img src="http://wow.zamimg.com/images/wow/icons/medium/trade_engineering.jpg"> <span>Видеомейкер</span></a>
+
+      </div>
+
+    </div>
+</div>
+<!--- СЛАЙДЕР --->
+
+
+
+<!--- Юзер-панель --->
+<div id="u-panel"></div>
+
+<script type="text/javascript">
+var arr=document.getElementsByTagName("div")
+i=0
+while(arr ){
+if(arr[i].id=="u-panel" && GroupID == "3") {
+name=arr[i].innerHTML
+name=name.substring(0)
+arr[i].innerHTML="<div id=option><span class='g-log'><b>Привет, Гость!</b></span><span class='u-ava'><img src=http://forumfiles.ru/files/0012/ad/ab/72354.jpg></span><div class='ug-data'>Регистрация позволит оставлять сообщения на форуме, прикрепить оружейную к профилю и подать заявки на вступление в гильдию или статики.<br/><br/>Незарегистрированные пользователи могут задать свои вопросы в разделе <a href='/viewforum.php?id=12'>Гостевая книга</a>.</div><div class='ug-mobile'><a href='login.php'>Вход</a><a href='register.php'>Регистрация</a></div></div>"+name
+}i++}
+</script>
+
+<!--- Последние посты --->
+<div id="lastp">
+<h3>Последние <span>сообщения</span></h3>
+
+<div class="lp-cont"></div>
+    <script src="http://bluewallkers.topbb.ru/export.php?type=js&max=6" type="text/javascript"></script>
+    <script type="text/javascript">
+    var html='';
+    var ts = '';
+    var mo = '';
+    var hh = '';
+    var mm = '';
+    var i = 0;
+    for(i=0;i<content.length;i++) {
+    ts = new Date();
+    ts.setTime(1000*content[i][0]);
+    mo = ts.getMonth()+1;
+    if (mo<10) mo='0'+mo;
+    hh = ts.getHours();
+    mm = ts.getMinutes()+'';
+    if (mm<10) mm='0'+mm;
+    ts = ts.getDate()+'.'+mo+' '+hh+':'+mm;
+    html2="</tr></tbody></table></div>"
+    html1="<div class=\"lp-table\"><table cellspacing=\"3\" width=100%><tbody class=\"hasicon\">"
+    html+='<tr><td colspan=\"2\"><a href=\"'+content[i][2]+'\" target=\"_blank\">'+content[i][3]+'</a><br><b>'+content[i][1]+'</b> - '+ts+'</td></tr>';
+    }
+
+    var arr=document.getElementsByTagName("div")
+    i=0
+    str=document.URL
+    while(arr[i] ){
+    if(document.getElementById("pun-index") && arr[i].className=="lp-cont"){
+    name=arr[i].innerHTML
+    name=name.substring(0)
+    arr[i].innerHTML=""+html1+""+html+""+html2+""+name+""
+    }
+    i++
+    }
+    </script>
+<a href="http://bluewallkers.topbb.ru/search.php?action=show_recent" id="allp">Все сообщения за сутки</a> </div>
+
+<!--- Стили и скрипт слайдера --->
+<style type="text/css">
+
+#vak {
+    display: inline-block;
+    margin: 4px 2px;
+    width: 200px;
+}
+#vak img {
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    border-radius: 5px;
+    margin-right: 3px;
+    width: 26px;
+}
+#vak span {
+    font-size: 18px;
+    position: relative;
+    top: -6px;
+}
+
+#recruit {
+    background: url("http://forumfiles.ru/files/0019/33/10/75074.png") no-repeat scroll 0 0 rgba(0, 0, 0, 0);
+    margin: 15px auto 0;
+    min-height: 140px;
+    width: 370px;
+}
+#tank, #heal, #dd {
+    display: block;
+    height: auto;
+    margin: 0 0 0 60px;
+    padding: 12px 0;
+    text-align: center;
+    vertical-align: top;
+    width: 290px;
+}
+#tank div, #heal div, #dd div {
+    border: 1px solid #7a7c71;
+    border-radius: 10px;
+    box-shadow: 0 0 3px #000;
+    display: inline-block;
+    height: 36px;
+    margin: 1px 3px;
+    width: 36px;
+}
+#war {background-image: url("http://forumfiles.ru/files/0019/33/10/69452.png"); background-repeat: no-repeat;}
+#dru {background-image: url("http://forumfiles.ru/files/0019/33/10/55426.png"); background-repeat: no-repeat;}
+#prist {background-image: url("http://forumfiles.ru/files/0019/33/10/21886.png"); background-repeat: no-repeat;}
+#mag {background-image: url("http://forumfiles.ru/files/0019/33/10/18919.png"); background-repeat: no-repeat;}
+#monk {background-image: url("http://forumfiles.ru/files/0019/33/10/54213.png"); background-repeat: no-repeat;}
+#hant {background-image: url("http://forumfiles.ru/files/0019/33/10/22043.png"); background-repeat: no-repeat;}
+#pal {background-image: url("http://forumfiles.ru/files/0019/33/10/70260.png"); background-repeat: no-repeat;}
+#rog {background-image: url("http://forumfiles.ru/files/0019/33/10/73736.png"); background-repeat: no-repeat;}
+#dk {background-image: url("http://forumfiles.ru/files/0019/33/10/10933.png"); background-repeat: no-repeat;}
+#lok {background-image: url("http://forumfiles.ru/files/0019/33/10/51464.png"); background-repeat: no-repeat;}
+#sham {background-image: url("http://forumfiles.ru/files/0019/33/10/81655.png"); background-repeat: no-repeat;}
+#dh {background-image: url("http://forumfiles.ru/files/0019/33/10/47970.png"); background-repeat: no-repeat;}
+#recruit .red {border: 1px solid #ff0000;}
+#recruit .yellow {border: 1px solid #fff600;}
+#recruit .green {border: 1px solid #43dd2c;}
+
+#slideshow {
+    height: 390px;
+    margin: 0 auto;
+    position: relative;
+    text-align: center;
+    width: 420px;
+    z-index: 40;
+}
+#slideshow #slidesContainer {
+  margin:0 auto;
+  width:420px;
+  height: 390px;
+  position:relative;
+}
+#slideshow #slidesContainer .slide {
+  margin:0 auto;
+  width:400px; /* На двадцать пикселев меньше чем ширина #slidesContainer */
+  height: 390px;
+}
+.control {
+    cursor: pointer;
+    display: block;
+    height: 164px;
+    position: absolute;
+    text-indent: -10000px;
+    top: 70px;
+    width: 130px;
+    z-index: 45;
+}
+
+#leftControl {
+  left: -85px;
+  background:transparent url(http://forumfiles.ru/files/0019/33/10/38701.png) no-repeat 0 0;
+}
+#leftControl:hover {background:transparent url(http://forumfiles.ru/files/0019/33/10/45635.png) no-repeat 0 0;}
+
+#rightControl {
+  right: -85px;
+  background:transparent url(http://forumfiles.ru/files/0019/33/10/61378.png) no-repeat 0 0;
+}
+#rightControl:hover {background:transparent url(http://forumfiles.ru/files/0019/33/10/98474.png) no-repeat 0 0;}
+
+#slideshow h3 {
+    color: #e8e361;
+    display: block;
+    font-size: 20px;
+    text-transform: uppercase;
+}
+
+</style>
+
+<script type="text/javascript">
+
+$(document).ready(function(){
+  var currentPosition = 0;
+  var slideWidth = 420;
+  var slides = $('.slide');
+  var numberOfSlides = slides.length;
+
+  // С помощью скрипта убираем прокрутку в контейнере.
+
+  $('#slidesContainer').css('overflow', 'hidden');
+  // Свернем все вкладки .slides в блок #slideInner 
+  slides .wrapAll('<div id="slideInner"></div>')
+	.css({
+      'float' : 'left',
+      'width' : slideWidth
+    });
+
+  // Проставим блоку #slideInner ширину, равную общей ширине контейнеров
+  $('#slideInner').css('width', slideWidth * numberOfSlides);
+  $('#slideshow')
+    .prepend('<span class="control" id="leftControl">Clicking moves left</span>')
+    .append('<span class="control" id="rightControl">Clicking moves right</span>');
+  manageControls(currentPosition);
+  $('.control')
+    .bind('click', function(){
+	currentPosition = ($(this).attr('id')=='rightControl') ? currentPosition+1 : currentPosition-1;
+    manageControls(currentPosition);
+    $('#slideInner').animate({
+      'marginLeft' : slideWidth*(-currentPosition)
+    });
+  });
+
+  function manageControls(position){
+    // Скрыть левую стрелку у первого контейнера
+	if(position==0){ $('#leftControl').hide() } else{ $('#leftControl').show() }
+	// Скрыть правую стрелку у последнего контейнера
+    if(position==numberOfSlides-1){ $('#rightControl').hide() } else{ $('#rightControl').show() }
+
+  }	
+});
+</script>
+		</div>
+	</div>
