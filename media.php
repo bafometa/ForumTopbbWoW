d8c112bcda9ab68004ffd990168dc112b0442507
Bafometa <35770842+bafometa@users.noreply.github.com>
2018-03-27 12:37:30 +0300

    Create media.php

---
+++ media.php
@@ -0,0 +1,210 @@
+<!-- banner_mini_top -->
+
+<style type="text/css">
+.pages div.container {overflow:hidden}
+</style>
+<div id="pun-main" class="main pages">
+	<div class="section">
+		<h1><span>Медиа</span></h1>
+		<div class="container">
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#wellcome]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#navig]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500); return false;});});</script>
+<script type="text/javascript">$(document).ready(function(){$( 'a[href^=#link-block]' ).click( function () {var id = $( this ).attr( 'href' ).match( /#.+$/ )[0].substr(1);	var offsetTop = $("a[name='"+ id + "']").offset().top;$('body,html').animate({scrollTop:offsetTop}, 500);return false;});});</script>
+
+
+<div id="pun-pageulinks"><ul class="container">
+<li><a href="#wellcome" class="some_link">Медиа-контент</a></li>
+<li><a href="#link-block" class="some_link">Стримы</a></li>
+<li><a href="#navig" class="some_link">Галерея: Скриншоты и Видео</a></li>
+</ul></div>
+
+
+<!-- верхний текст -->
+<div id="text-block" style="">
+    <a name="wellcome"></a>
+    <span class="up-h2">Видео, стримы, соц-сети</span>
+    <h2>Наше медиа-пространство</h2>
+</div>
+
+<div id="media_block">
+
+<div class="left">
+Гильдия «Синие Ходоки» обладает командой, заинтересованной в медиа-продвижении нашего сообщества. Мы пилим видосы, создаем мемы, занимаемся артом, дизайном, программированием и т.д. Плоды нашего совместного творчества, а также внутриигровую историю гильдии вы можете лицезреть здесь. =)<br>
+<br>
+<a href="https://www.youtube.com/channel/UCImgqZedOhQtuzI_mZpUQcw" target="_blank" class="tip" title="Наш канал на YouTube"><img src="http://s6.uploads.ru/zSDoO.png"></a><br>
+
+<a href="https://vk.com/bluewalkersalalliance" target="_blank" class="tip" title="Наш паблик Вконтакте"><img src="http://forumfiles.ru/files/0019/33/10/89359.png"></a><br>
+
+</div>
+
+<div class="right">
+
+<a href="#link-block" class="some_link tip" title="Наши стриммеры"><img src="http://forumfiles.ru/files/000f/29/31/24597.png"></a><br>
+<div id="tab-block">
+  <ul class="tabs">
+    <li id="pic">
+      <a href="https://www.twitch.tv/bafometa" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+      <a href="https://www.twitch.tv/bafometa" class="act tip" title="Бафомета: блад дк/мишка"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+    </li>
+  </ul>
+</div>
+
+
+</div>
+
+</div>
+
+<hr class="big-hr1">
+
+<!-- Стриммеры -->
+<div id="text-block" style="padding-top: 55px;">
+    <a name="link-block"></a>
+    <span class="up-h2">Наши стриммеры</span>
+    <h2>Подпишись на наших стриммеров!</h2>
+</div>
+
+<div id="tab-block" style="margin-top: -30px;">
+
+<!-- меню -->
+  <ul class="tabs">
+    <li class="active" id="pic">
+      <a href="#tab2" class="no-act"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+      <a href="#tab2" class="act tip" title="Бафомета: танк дк"><img src="https://render-eu.worldofwarcraft.com/character/galakrond/8/136863240-avatar.jpg"></a>
+    </li>
+   
+  </ul>
+
+  <div class="tab_container1">
+
+<!-- Бафомета -->
+    <div  style="display: block;" id="tab2" class="tab_content">
+
+<div id="str">
+   <div id="info-str">
+          <span class="up-h2">Блад дк/ретропал</span>
+          <h3>Бафомета</h3>
+          <a href="/profile.php?id=2">Профиль</a> • <a href="http://eu.battle.net/wow/ru/character/galakrond/%D0%91%D0%B0%D1%84%D0%BE%D0%BC%D0%B5%D1%82%D0%B0/advanced" target="_blank">Армори</a><br/><br/>
+          <span class="logo">
+          <a href="https://www.youtube.com/channel/UCImgqZedOhQtuzI_mZpUQcw" class="tip" title="Стрим на YouTube" target="_blank"><img src="http://s4.uploads.ru/Z1q9J.png"></a>
+          <a href="https://www.twitch.tv/bafometa" class="tip" title="Стрим на Twitch" target="_blank"><img src="http://s9.uploads.ru/ImhxM.png"></a>
+           </span>
+   </div>
+   <div id="str-win"><iframe src="https://player.twitch.tv/?channel=bafometa" frameborder="0" scrolling="no" height="316" width="600"></iframe>
+   </div>
+</div>
+
+    </div>
+
+<!-- конец -->
+  </div>
+</div>
+<hr class="big-hr2">
+
+<!-- Галерея -->
+<div id="text-block" style="padding-top: 75px;">
+    <a name="navig"></a>
+    <span class="up-h2">Наши видео и скриншоты</span>
+    <h2>Отчеты о наших приключениях</h2>
+</div>
+<div id="tab-block" style="margin-top: -30px;">
+
+<!-- меню -->
+  <ul class="tabs2">
+    <li class="active"><a href="#tab100">Хроники</a></li>
+    <li><a href="#tab101">Актуальные PvE рейды</a></li>
+    <li><a href="#tab102">PvE архив</a></li>
+  </ul>
+
+  <div class="tab_container2">
+
+<!-- Хроники -->
+    <div style="display: block;" id="tab100" class="tab2_content">
+
+<h2>Видео Хроники   (<a href="https://www.youtube.com/channel/UCImgqZedOhQtuzI_mZpUQcw" target="_blank">Все видео</a>)</h2>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<!--<div id="gal-item">
+<a class="gall-no" href="#"><img src="http://s5.uploads.ru/P5FVC.jpg"></a>
+<a class="gallery tip" title="Feign Death или как скинуть агро с мобов, но не с РЛов" href="#testube6"><img src="http://s5.uploads.ru/P5FVC.jpg" /></a> 
+<div style="display:none" id="testube6"><iframe width="910" height="510" src="https://www.youtube.com/embed/ZrFNK64qYo0" frameborder="0" allowfullscreen></iframe></div>
+</div>-->
+
+
+<h2>Памятные скриншоты</h2>
+<!--<div id="gal-item">
+<a class="gall-no" href="#"><img src="http://s3.uploads.ru/ClbiX.jpg"></a>
+<a class="gallery tip" href="http://s3.uploads.ru/ClbiX.jpg" title="Рейды в ЛЧГ." rel="group2"><img src="http://s3.uploads.ru/ClbiX.jpg"></a>
+</div>-->
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+
+    </div>
+
+<!-- Актуальное PvE -->
+    <div style="display: none;" id="tab101" class="tab2_content">
+
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+
+    </div>
+
+<!-- PvE архив -->
+    <div style="display: none;" id="tab102" class="tab2_content">
+
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+<div id="gal-item"><a class="gall-no" href="#navig"><img src="http://forumfiles.ru/files/000f/29/31/19350.png"></a></div>
+
+
+    </div>
+
+  </div>
+</div>
+
+
+
+
+<!-- Скрипт вкладок  -->
+<script type="text/javascript">
+$(document).ready(function() {
+  $(".tab_content").hide();  
+  $("ul.tabs li:first").addClass("active").show();
+  $(".tab_content:first").show();   
+  $("ul.tabs li").click(function() {
+  $("ul.tabs li").removeClass("active");
+  $(this).addClass("active");
+  $(".tab_content").hide();
+  var activeTab = $(this).find("a").attr("href");
+  $(activeTab).fadeIn();
+  return false;
+  });
+});
+</script>
+<script type="text/javascript">
+$(document).ready(function() {
+  $(".tab2_content").hide();  
+  $("ul.tabs2 li:first").addClass("active").show();
+  $(".tab2_content:first").show();   
+  $("ul.tabs2 li").click(function() {
+  $("ul.tabs2 li").removeClass("active");
+  $(this).addClass("active");
+  $(".tab2_content").hide();
+  var activeTab = $(this).find("a").attr("href");
+  $(activeTab).fadeIn();
+  return false;
+  });
+});
+</script>
+<style>#pun-pages .main {height: 2300px;}
+#tab2 {background: rgba(0, 0, 0, 0) url("http://s0.uploads.ru/XqKLt.jpg") no-repeat scroll 0 0;}
+#tab4 {background: rgba(0, 0, 0, 0) url("http://sh.uploads.ru/g2Bpb.jpg") no-repeat scroll 0 0;}
+#tab5 {background: rgba(0, 0, 0, 0) url("http://se.uploads.ru/LfQNA.jpg") no-repeat scroll 0 0;}
+</style>
+		</div>
+	</div>
+</div>
+<script type="text/javascript">$(document).trigger("pun_main_ready");</script>
+
